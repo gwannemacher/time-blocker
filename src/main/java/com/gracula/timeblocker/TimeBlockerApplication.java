511dde6b6e1a03d3826a2055ed5a7ca2e7a60c88
Grace Wannemacher <gwannemacher@gmail.com>
2022-01-26 08:39:38 -0500

    Add Spring project

---
+++ src/main/java/com/gracula/timeblocker/TimeBlockerApplication.java
@@ -0,0 +1,13 @@
+package com.gracula.timeblocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeBlockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gracula/timeblocker/TimeBlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gracula.timeblocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeBlockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
