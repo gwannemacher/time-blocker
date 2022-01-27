39953cdf887d779aaf72c364fbd146aea5de61fe
Grace Wannemacher <gwannemacher@gmail.com>
2022-01-27 08:39:33 -0500

    Minor calendar config, add interaction plugin

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import timeGridPlugin from '@fullcalendar/timegrid' // a plugin!
+import FullCalendar from '@fullcalendar/react'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 function App() {
   return (
     <FullCalendar
       plugins={[ timeGridPlugin ]}
       initialView="timeGridWeek"
+      nowIndicator={true}
+      scrollTime="07:00:00"
+      dayHeaderFormat={{weekday: 'short', day: 'numeric'}}
     />
   );
 }
