566635b96ca21bc68e6d4ecef6ca55fd646adbb5
Grace Wannemacher <gwannemacher@gmail.com>
2022-01-27 08:23:09 -0500

    Add FullCalendar to App.js!

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import timeGridPlugin from '@fullcalendar/timegrid' // a plugin!
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FullCalendar
+      plugins={[ timeGridPlugin ]}
+      initialView="timeGridWeek"
+    />
   );
 }
 
