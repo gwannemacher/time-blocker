68adfa0b9d9a041986f72c8448c9aa221178405a
Grace Wannemacher <gwannemacher@gmail.com>
2022-01-27 08:40:59 -0500

    Add date click interaction

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -6,11 +6,12 @@ import interactionPlugin from '@fullcalendar/interaction'
 function App() {
   return (
     <FullCalendar
-      plugins={[ timeGridPlugin ]}
+      plugins={[ timeGridPlugin, interactionPlugin ]}
       initialView="timeGridWeek"
       nowIndicator={true}
       scrollTime="07:00:00"
       dayHeaderFormat={{weekday: 'short', day: 'numeric'}}
+      dateClick={(info) => { console.log('clicked on', info.dateStr)}}
     />
   );
 }
