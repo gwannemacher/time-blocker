b51914eb13a72998fc95ef83b2fdaeb6e02b06bd
Grace Wannemacher <gwannemacher@gmail.com>
2022-01-29 15:10:12 -0500

    add modal to be opened on date click. copied css over from bootstrap so the modal would actually show

--- src/client/src/calendar.js
+++ src/client/src/calendar.js
@@ -2,8 +2,17 @@ import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import Modal from 'react-bootstrap/Modal'
+import ModalDialog from 'react-bootstrap/ModalDialog'
+import ModalHeader from 'react-bootstrap/ModalHeader'
+import ModalTitle from 'react-bootstrap/ModalTitle'
+import ModalBody from 'react-bootstrap/ModalBody'
+import ModalFooter from 'react-bootstrap/ModalFooter'
+import Form from 'react-bootstrap/Form'
 
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/calendar.css';
+import './stylesheets/modal.css';
 
 const toCalendarEvent = (date, title) => {
     const [year, month, day, hours] = [date.getFullYear(), date.getMonth(), date.getDate(), date.getHours()];
@@ -50,23 +59,59 @@ const eventClick = (info) => {
 
 const Calendar = () => {
     const [events, setEvents] = useState([]);
+    const [isFormVisible, setIsFormVisible] = useState(false);
+
+    const onDateClick = (info, showForm) => {
+        showForm();
+
+        console.log('haha');
+
+//        const newEvent = info.allDay
+//            ? toAllDayCalendarEvent(info.date, title)
+//            : toCalendarEvent(info.date, title);
+//
+//        info.view.calendar.addEvent(newEvent);
+    }
 
     return (
-        <FullCalendar
-            plugins={[ timeGridPlugin, interactionPlugin ]}
-            initialView="timeGridWeek"
-            nowIndicator={true}
-            scrollTime="08:00:00"
-            dayHeaderFormat={{weekday: 'long', day: 'numeric'}}
-            stickyHeaderDates={true}
-            titleFormat={{ year: 'numeric', month: 'long', day: 'numeric' }}
-            dateClick={dateClick}
-            events={events}
-            eventClick={eventClick}
-            editable={true}
-            eventResizableFromStart={true}
-            eventTimeFormat={{hour: 'numeric', minute: '2-digit', meridiem: 'narrow'}}
-        />
+        <>
+            <Modal
+                show={isFormVisible}
+                onHide={() => setIsFormVisible(false)}
+            >
+                <Modal.Header closeButton>
+                  <Modal.Title>
+                    New time block
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                      <Form.Group>
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" placeholder="title" />
+                      </Form.Group>
+                      <button type="submit">
+                        Submit
+                      </button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+            <FullCalendar
+                plugins={[ timeGridPlugin, interactionPlugin ]}
+                initialView="timeGridWeek"
+                nowIndicator={true}
+                scrollTime="08:00:00"
+                dayHeaderFormat={{weekday: 'long', day: 'numeric'}}
+                stickyHeaderDates={true}
+                titleFormat={{ year: 'numeric', month: 'long', day: 'numeric' }}
+                dateClick={(info) => onDateClick(info, () => setIsFormVisible(true))}
+                events={events}
+                eventClick={eventClick}
+                editable={true}
+                eventResizableFromStart={true}
+                eventTimeFormat={{hour: 'numeric', minute: '2-digit', meridiem: 'narrow'}}
+            />
+        </>
     );
 }
 
