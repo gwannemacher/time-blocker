936bc29c3c392646a23da3eb07cee08ca0cbe1d0
Grace Wannemacher <gwannemacher@gmail.com>
2022-02-03 19:26:59 -0500

    add apollo stuff

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
 import './stylesheets/index.css';
 import App from './App';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8080/graphql',
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'cache-and-network',
+        },
+    },
+});
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
 );
