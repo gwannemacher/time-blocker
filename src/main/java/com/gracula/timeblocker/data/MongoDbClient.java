d18aafafed71c0da7aeb90843c3d9d73347be79d
Grace Wannemacher <gwannemacher@gmail.com>
2022-02-05 13:13:04 -0500

    omg

--- src/main/java/com/gracula/timeblocker/data/MongoDbClient.java
+++ src/main/java/com/gracula/timeblocker/data/MongoDbClient.java
@@ -17,40 +17,22 @@ import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 @Component
-@PropertySource("classpath:application.properties")
+@PropertySource({"classpath:.env", "classpath:application.properties"})
 public class MongoDbClient {
     public final MongoCollection<TimeBlock> timeBlockCollection;
 
-    public MongoDbClient() {
-        System.out.println("**********");
-        System.out.println(System.getenv("MONGO_USERNAME"));
-        System.out.println("**********");
-
-        System.getenv().forEach((k, v) -> {
-            System.out.println(k + ":" + v);
-        });
-
-
-        final String mongoDbUrl = String.format(
-                "mongodb+srv://%s:%s@%s.%s.mongodb.net/%s?retryWrites=true&w=majority",
-                System.getenv("MONGO_USERNAME"),
-                System.getenv("MONGO_PASSWORD"),
-                System.getenv("MONGO_CLUSTER_NAME"),
-                System.getenv("MONGO_CLUSTER_THINGY"),
-                System.getenv("MONGO_DB_NAME_THINGY"));
-
-        System.out.println("--------");
-        System.out.println(mongoDbUrl);
-        System.out.println("--------");
+    public MongoDbClient(@Value("${mongodb.url}") String mongoDbUrl,
+                         @Value("${mongodb.database.name}") String databaseName,
+                         @Value("${mongodb.collection.time_block.name}") String timeBlockCollectionName) {
         final CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
         final CodecRegistry pojoCodecRegistry = fromRegistries(
                 getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
 
         final MongoClient mongoClient = MongoClients.create(mongoDbUrl);
         final MongoDatabase database = mongoClient
-                .getDatabase(System.getenv("MONGO_DB_NAME"))
+                .getDatabase(databaseName)
                 .withCodecRegistry(pojoCodecRegistry);
 
-        timeBlockCollection = database.getCollection("time_blocks", TimeBlock.class);
+        timeBlockCollection = database.getCollection(timeBlockCollectionName, TimeBlock.class);
     }
 }
