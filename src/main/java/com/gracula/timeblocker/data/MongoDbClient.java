ebfecc265dd2c33d99ef5a981b0596b8a9c81cdc
Grace Wannemacher <gwannemacher@gmail.com>
2022-02-05 12:42:39 -0500

    troubleshooting heroku omg

--- src/main/java/com/gracula/timeblocker/data/MongoDbClient.java
+++ src/main/java/com/gracula/timeblocker/data/MongoDbClient.java
@@ -21,19 +21,36 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 public class MongoDbClient {
     public final MongoCollection<TimeBlock> timeBlockCollection;
 
-    public MongoDbClient(
-            @Value("${mongodb.url}") String mongoDbUrl,
-            @Value("${mongodb.database.name}") String databaseName,
-            @Value("${mongodb.collection.time_block.name}") String timeBlockCollectionName) {
+    public MongoDbClient() {
+        System.out.println("**********");
+        System.out.println(System.getenv("MONGO_USERNAME"));
+        System.out.println("**********");
+
+        System.getenv().forEach((k, v) -> {
+            System.out.println(k + ":" + v);
+        });
+
+
+        final String mongoDbUrl = String.format(
+                "mongodb+srv://%s:%s@%s.%s.mongodb.net/%s?retryWrites=true&w=majority",
+                System.getenv("MONGO_USERNAME"),
+                System.getenv("MONGO_PASSWORD"),
+                System.getenv("MONGO_CLUSTER_NAME"),
+                System.getenv("MONGO_CLUSTER_THINGY"),
+                System.getenv("MONGO_DB_NAME_THINGY"));
+
+        System.out.println("--------");
+        System.out.println(mongoDbUrl);
+        System.out.println("--------");
         final CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
         final CodecRegistry pojoCodecRegistry = fromRegistries(
                 getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
 
         final MongoClient mongoClient = MongoClients.create(mongoDbUrl);
         final MongoDatabase database = mongoClient
-                .getDatabase(databaseName)
+                .getDatabase(System.getenv("MONGO_DB_NAME"))
                 .withCodecRegistry(pojoCodecRegistry);
 
-        timeBlockCollection = database.getCollection(timeBlockCollectionName, TimeBlock.class);
+        timeBlockCollection = database.getCollection("time_blocks", TimeBlock.class);
     }
 }
