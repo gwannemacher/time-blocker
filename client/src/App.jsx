b1d84d8b42e8d9f6d8e9af66cc3e4da20aaf4ca7
Grace Wannemacher <gwannemacher@gmail.com>
2022-05-22 17:58:20 -0400

    - delete HoveredEvent - rid rid of App middleman and just import CalendarApp to index.jsx

--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './stylesheets/App.css';
-import CalendarApp from './components/calendar-app';
-
-function App() {
-    return <CalendarApp />;
-}
-
-export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,8 +9,7 @@ import {
 
 import './stylesheets/index.css';
 import EventTypes from './models/event-types';
-import App from './App';
-import { selectedVar } from './components/calendar-app';
+import CalendarApp, { selectedVar } from './components/calendar-app';
 
 const httpLink = createHttpLink({
     // uri: 'http://localhost:5000/graphql',
@@ -51,7 +50,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <ApolloProvider client={client}>
-            <App />
+            <CalendarApp />
         </ApolloProvider>
     </React.StrictMode>
 );
