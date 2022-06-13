7ef17388ed46c8c305bf01146a3b6faa61c450e2
Grace Wannemacher <gwannemacher@gmail.com>
2022-06-13 19:44:47 -0400

    move more logic into calendar and refactor the event handlers

--- client/src/components/calendar-app.jsx
+++ client/src/components/calendar-app.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { gql, useApolloClient, makeVar } from '@apollo/client';
+import { makeVar } from '@apollo/client';
 import * as dayjs from 'dayjs';
 
 import Calendar from './calendar';
 import Form from './event-form/form';
-import useUpdateTimeBlockTimes from '../hooks/useUpdateTimeBlockTimes';
 import useGetTimeBlocksInRange from '../hooks/useGetTimeBlocks';
 
 import '../stylesheets/calendar.css';
@@ -12,14 +11,12 @@ import '../stylesheets/calendar.css';
 export const selectedVar = makeVar({ id: '', isSelected: false });
 
 function CalendarApp() {
-    const client = useApolloClient();
     const today = dayjs(new Date()).startOf('day');
     const [range, setRange] = useState({
         start: today.startOf('week').valueOf(),
         end: today.add(1, 'week').valueOf(),
     });
     const { data } = useGetTimeBlocksInRange(range.start, range.end);
-    const [updateTimeBlockTimes] = useUpdateTimeBlockTimes();
     const [selectedDateInfo, setSelectedDateInfo] = useState(null);
 
     const onDateClick = (info) => {
@@ -36,32 +33,6 @@ function CalendarApp() {
         }
     };
 
-    const onEventTimeChange = (info) => {
-        const { id, start, end } = info.event;
-
-        updateTimeBlockTimes({
-            variables: {
-                id,
-                startDateTime: dayjs(start).valueOf(),
-                endDateTime: dayjs(end).valueOf(),
-            },
-        });
-
-        client.writeFragment({
-            id: `TimeBlock:${id}`,
-            fragment: gql`
-                fragment UpdatedTimeBlock on TimeBlock {
-                    startDateTime
-                    endDateTime
-                }
-            `,
-            data: {
-                startDateTime: dayjs(start).valueOf(),
-                endDateTime: dayjs(end).valueOf(),
-            },
-        });
-    };
-
     return (
         <>
             <Form
@@ -74,7 +45,6 @@ function CalendarApp() {
                 timeBlocks={data?.getTimeBlocksInRange}
                 onEventClick={onEventClick}
                 onDateClick={onDateClick}
-                onEventTimeChange={onEventTimeChange}
                 setRange={setRange}
             />
         </>
--- client/src/components/calendar.jsx
+++ client/src/components/calendar.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from 'react';
+import { gql, useApolloClient } from '@apollo/client';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -6,62 +7,85 @@ import * as dayjs from 'dayjs';
 
 import EventTypes from '../models/event-types';
 import isPastEvent from '../utilities/time-utilities';
+import useUpdateTimeBlockTimes from '../hooks/useUpdateTimeBlockTimes';
 
 import '../stylesheets/calendar.css';
 
-function Calendar(props) {
-    const {
-        timeBlocks,
-        onEventClick,
-        onDateClick,
-        onEventTimeChange,
-        setRange,
-    } = props;
+const rangeForThisWeek = () => {
+    const today = dayjs(new Date()).startOf('day');
+    return {
+        start: today.startOf('week').valueOf(),
+        end: today.add(1, 'week').valueOf(),
+    };
+};
 
+const rangeForPreviousWeek = (calendarApi: any) => ({
+    start: dayjs(calendarApi.view.activeStart).subtract(1, 'week').valueOf(),
+    end: dayjs(calendarApi.view.activeEnd).subtract(1, 'week').valueOf(),
+});
+
+const rangeForNextWeek = (calendarApi: any) => ({
+    start: dayjs(calendarApi.view.activeStart).add(1, 'week').valueOf(),
+    end: dayjs(calendarApi.view.activeEnd).add(1, 'week').valueOf(),
+});
+
+function Calendar(props) {
+    const { timeBlocks, onEventClick, onDateClick, setRange } = props;
+    const client = useApolloClient();
     const calendarRef = useRef(null);
+    const [updateTimeBlockTimes] = useUpdateTimeBlockTimes();
+
+    const onEventTimeChange = (info) => {
+        const { id, start, end } = info.event;
+
+        updateTimeBlockTimes({
+            variables: {
+                id,
+                startDateTime: dayjs(start).valueOf(),
+                endDateTime: dayjs(end).valueOf(),
+            },
+        });
+
+        client.writeFragment({
+            id: `TimeBlock:${id}`,
+            fragment: gql`
+                fragment UpdatedTimeBlock on TimeBlock {
+                    startDateTime
+                    endDateTime
+                }
+            `,
+            data: {
+                startDateTime: dayjs(start).valueOf(),
+                endDateTime: dayjs(end).valueOf(),
+            },
+        });
+    };
 
     useEffect(() => {
+        const calendarApi = calendarRef.current.getApi();
+
         const setTimeRangeForPrevious = () => {
-            const calendarApi = calendarRef.current.getApi();
-            setRange({
-                start: dayjs(calendarApi.view.activeStart)
-                    .subtract(1, 'week')
-                    .valueOf(),
-                end: dayjs(calendarApi.view.activeEnd)
-                    .subtract(1, 'week')
-                    .valueOf(),
-            });
+            const { start, end } = rangeForPreviousWeek(calendarApi);
+            setRange({ start, end });
+        };
+
+        const setTimeRangeForNext = () => {
+            const { start, end } = rangeForNextWeek(calendarApi);
+            setRange({ start, end });
+        };
+
+        const setTimeRangeForThisWeek = () => {
+            const { start, end } = rangeForThisWeek();
+            setRange({ start, end });
         };
 
         document
             .querySelector('[title="Previous week"]')
             .addEventListener('click', setTimeRangeForPrevious);
-    }, []);
-
-    useEffect(() => {
-        const setTimeRangeForNext = () => {
-            const calendarApi = calendarRef.current.getApi();
-            setRange({
-                start: dayjs(calendarApi.view.activeStart)
-                    .add(1, 'week')
-                    .valueOf(),
-                end: dayjs(calendarApi.view.activeEnd).add(1, 'week').valueOf(),
-            });
-        };
 
         document
             .querySelector('[title="Next week"]')
             .addEventListener('click', setTimeRangeForNext);
-    }, []);
-
-    useEffect(() => {
-        const setTimeRangeForThisWeek = () => {
-            const today = dayjs(new Date()).startOf('day');
-            setRange({
-                start: today.startOf('week').valueOf(),
-                end: today.add(1, 'week').valueOf(),
-            });
-        };
 
         document
             .querySelector('[title="This week"]')
